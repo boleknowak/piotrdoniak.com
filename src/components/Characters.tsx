ade8e3f4d07bc3741719667065a52ebdfdc288eb
boleknowak <boleknowak@o2.pl>
2023-03-05 15:21:46 +0100

    :chart_with_upwards_trend: added google analytics

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,6 +1,17 @@
 import Image from 'next/image';
+import * as gtag from '@/lib/gtag';
+import { useEffect } from 'react';
 
 export default function Characters({ data }) {
+  useEffect(() => {
+    gtag.event({
+      action: 'view_characters',
+      category: 'Characters',
+      label: 'View Characters',
+      value: data?.results.length,
+    });
+  }, [data]);
+
   return (
     <div className="grid grid-cols-4 gap-4">
       {data?.results.map((character) => (

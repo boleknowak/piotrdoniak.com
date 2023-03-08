7096900e0c74922992862eb2577569962fc55455
boleknowak <boleknowak@o2.pl>
2023-03-08 16:04:21 +0100

    :hammer: one more test

--- src/pages/api/characters.ts
+++ src/pages/api/characters.ts
@@ -1,4 +1,4 @@
-import delay from '@/lib/delay';
+// import delay from '@/lib/delay';
 import type { NextApiRequest, NextApiResponse } from 'next';
 // import { getServerSession } from 'next-auth/next';
 // import { authOptions } from '@/pages/api/auth/[...nextauth]';
@@ -12,7 +12,7 @@ export default async function handle(request: NextApiRequest, response: NextApiR
   // }
 
   const res = await fetch('https://rickandmortyapi.com/api/character');
-  await delay(2000);
+  // await delay(2000);
   const data = await res.json();
 
   response.json({ data });

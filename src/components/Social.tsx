f7d43c38f5d33a1226cc845410778cb057047761
boleknowak <boleknowak@o2.pl>
2023-03-12 22:03:25 +0100

    :tada: added social component

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Social({ social }) {
+export default function Social({ social, source = 'other' }) {
+  let { url } = social;
+  url = url.replace('{source}', source);
+
   return (
-    <a href={social.url} target="_blank" className="social-item block">
+    <a href={url} target="_blank" className="social-item block">
       <div className="flex flex-row items-center space-x-2 rounded-md border border-gray-200 bg-white p-4 transition duration-300 hover:scale-105 hover:bg-gray-100">
         <FontAwesomeIcon color={social.color} icon={social.icon} size="lg" className="w-4" />
         <div>{social.text}</div>
---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function SocialIcon({ social, source = 'other' }) {
+  let { url } = social;
+  url = url.replace('{source}', source);
+
+  return (
+    <a href={url} target="_blank" className="social-icon-item block">
+      <div className="flex h-8 w-8 items-center justify-center rounded-md border border-gray-200 bg-white hover:bg-gray-100">
+        <FontAwesomeIcon color={social.color} icon={social.icon} size="lg" className="w-4" />
+      </div>
+    </a>
+  );
+}

f6caecf90e4c498fe7a62f08fe7930200e5594dd
boleknowak <boleknowak@o2.pl>
2023-04-23 12:34:19 +0200

    :hammer: fix next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: 'experimental-edge',
-  },
+  runtime: 'experimental-edge',
   // productionBrowserSourceMaps: true,
   images: {
     remotePatterns: [
